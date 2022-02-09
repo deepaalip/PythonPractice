0ae73417cdc2f00751d42b4bb19b3e6e6b5a270b
deepali <deepaalipatil@gmail.com>
2022-02-09 23:26:40 +0530

    ADDED:print string with proper username

---
+++ pythonCoreProgram/UserInput.py
@@ -0,0 +1,13 @@
+
+import re
+
+userName = input('Enter your name: ')
+string = " Hello " +userName+ ", How are you?" 
+newUserName = input("enter new UserName:")
+newString = string.replace(userName,newUserName)
+Match = re.match('[A-Z]{1}[a-z]{2,}$',newUserName)
+if Match:
+    print(newString)
+else:
+    print('Invalid UserName')
+
