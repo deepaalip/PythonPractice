74242169ca846f8fd10ba8df645f952b06c97ec7
deepali <deepaalipatil@gmail.com>
2022-02-11 08:23:50 +0530

    UPDATED: Find roots of quadratic equation

--- FunctionalProgram/Quadratic.py
+++ FunctionalProgram/Quadratic.py
@@ -1,10 +1,12 @@
 import cmath
-a = int(input('Enter value of a:'))
-b = int(input('Enter  value of b:'))
-c = int(input('Enter  value of c:'))
-
-delta = (int(b*b) - (4*a*c))
+def getRootOfQuadraticEquation(a, b, c): 
+ delta = (int(b*b) - (4*a*c))
 
 root1_Of_x = (-b + cmath.sqrt(delta))/(2*a)
 root2_Of_x = (-b - cmath.sqrt(delta))/(2*a)
 print("Solution to the equation are:",root1_Of_x,root2_Of_x)
+
+a = int(input('Enter value of a:'))
+b = int(input('Enter  value of b:'))
+c = int(input('Enter  value of c:'))
+getRootOfQuadraticEquation(a, b, c)
