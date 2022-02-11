5e614ae3122559ab9712194d42900d15a5bd30e4
deepali <deepaalipatil@gmail.com>
2022-02-11 08:23:14 +0530

    UPDATED: Find euclidean distance between two points

--- FunctionalProgram/Distance.py
+++ FunctionalProgram/Distance.py
@@ -1,15 +1,19 @@
-
 import math
+def findDistance(point_x1,point_y1):
 
-x1 = int(input("Enter the First Point Coordinate x1 = "))
-y1 = int(input("Enter the First Point Coordinate y1  = "))
+          x = math.pow((point_x1), 2)
+          y = math.pow((point_y1), 2)
 
-x = math.pow((x1), 2)
-y = math.pow((y1), 2)
+          print(x)
+          print(y)
+          print(math.sqrt(x + y))
+          distance = math.sqrt(x + y)
+          
+          print('The Distance Between Two Points = {0} Units'.format(distance))
+    
+point_x1 = int(input("Enter the First Point Coordinate x1 = "))
+point_y1 = int(input("Enter the First Point Coordinate y1  = "))
 
-print(x)
-print(y)
-print(math.sqrt(x + y))
-distance = math.sqrt(x + y)
+findDistance(point_x1, point_y1)
 
-print('The Distance Between Two Points = {0} Units'.format(distance))
+   
