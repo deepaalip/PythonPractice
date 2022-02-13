553643ab47e4e0c73019a136bd8a33c67a660078
deepali <deepaalipatil@gmail.com>
2022-02-13 16:08:29 +0530

    ADDED:Perform pytest for vending machine

---
+++ JUnitprogram/test_vendingmachine.py
@@ -0,0 +1,17 @@
+import pytest
+
+def countCurrency(amount):     
+    notes = [2000,1000,500, 200, 100,
+               50, 20, 10, 5, 1]
+                
+    noteCounter = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+     
+    print ("Currency Count -> ")     
+    for i, j in zip(notes, noteCounter):
+        if amount >= i:
+            j = amount // i
+            amount = amount - j * i
+            print (i ," : ", j)
+ 
+amount = 1800
+countCurrency(amount)
